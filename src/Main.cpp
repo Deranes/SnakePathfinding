9c47547486a4cf912358126b23d5db91ebddad0a
Ola Enberg <deranes21@hotmail.com>
2015-11-25 08:39:52 +0100

    Set up a basic (Hello World) Visual Studio 2015 project.

---
+++ src/Main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+	std::cout << "Hello World!" << std::endl;
+	std::getchar();		// Pauses the application so that the output can be read.
+
+	return 0;	// Exit success.
+};
