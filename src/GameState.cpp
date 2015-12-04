3e302674312b4e1c1226978879fa4f619cbfee5f
Ola Enberg <deranes21@hotmail.com>
2015-12-04 17:20:34 +0100

    Snakes now spawn on different locations instead of all on the same tile.

--- src/GameState.cpp
+++ src/GameState.cpp
@@ -23,13 +23,17 @@ GameState::GameState( const glm::uvec2& size, size_t nrOfTeams, size_t snakesPer
 
 	// Initialize teams and snakes.
 	this->Teams.resize( nrOfTeams );				// Create all teams.
-	for ( auto& team : this->Teams ) {
+	for ( size_t teamIndex = 0; teamIndex < this->Teams.size(); ++teamIndex ) {
+		Team& team		= this->Teams[teamIndex];
+
 		// Initialize the snakes.
 		team.Snakes.resize( snakesPerTeam );		// Create all snakes in each team.
-		for ( auto& snake : team.Snakes ) {
-			snake.SegmentsToSpawn								= snakeLength - 1;			// Only the snakes head is on the board at the start, rest of the body gets spawned later.
-			const glm::ivec2 spawnPosition						= glm::ivec2( 5, 5 );		// Arbitrary spawn position.					// TODO: Remove magic numbers.
-			this->Board[spawnPosition.y][spawnPosition.x]		= Tile::Blocked;			// Block the snakes position in the board.
+		for ( size_t snakeIndex = 0; snakeIndex < team.Snakes.size(); ++snakeIndex ) {
+			Snake& snake										= team.Snakes[snakeIndex];
+			snake.SegmentsToSpawn								= snakeLength - 1;									// Only the snakes head is on the board at the start, rest of the body gets spawned later.
+			const glm::ivec2 spawnPosition						= glm::ivec2(	5 + snakeIndex * snakeLength,		// Arbitrary spawn position.					// TODO: Remove magic numbers.
+																				5 + teamIndex * snakeLength );					
+			this->Board[spawnPosition.y][spawnPosition.x]		= Tile::Blocked;									// Block the snakes position in the board.
 			snake.Segments.push_back( spawnPosition );
 		}
 	}
